a4238321a473b754e6b1fca94e2c57c0bd8f2c29
Kenji Okimoto <okimoto@clear-code.com>
2013-03-06 18:40:33 +0900

    Remove unused methods

--- lib/commit-comment-tools/repository-stats.rb
+++ lib/commit-comment-tools/repository-stats.rb
@@ -101,46 +101,5 @@ module CommitCommentTools
         CommitGroup.new(branch_name, key, commits)
       end
     end
-
-    def group_by_key(branch_name, key="%Y-%m-%d")
-      # TODO Use Grit::Commit.find_all
-      groups = @repository.commits(branch_name, nil).group_by do |commit|
-        commit.date.strftime(key)
-      end
-      groups.each do |k, commits|
-        yield k, commits
-      end
-    end
-
-    def commit_count(branch_name, key="%Y-%m-%d")
-      # TODO fix variable name
-      h = {}
-      group_by_key(branch_name, key) do |k, commits|
-        h[k] = commits.size
-      end
-      h
-    end
-
-    def commit_diff_lines(branch_name, key="%Y-%m-%d")
-      # TODO fix variable name
-      h = {}
-      group_by_key(branch_name, key) do |k, commits|
-        h[k] = commits.inject(0) do |memo, commit|
-          memo + commit.diff_lines_count
-        end
-      end
-      h
-    end
-
-    def commit_diff_bytesize(branch_name, key="%Y-%m-%d")
-      # TODO fix variable name
-      h = {}
-      group_by_key(branch_name, key) do |k, commits|
-        h[k] = commits.inject(0) do |memo, commit|
-          memo + commit.diff_bytesize
-        end
-      end
-      h
-    end
   end
 end
