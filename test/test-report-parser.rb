8a944acaaafc44ad1d452eb017388f024248e506
Kenji Okimoto <okimoto@clear-code.com>
2013-04-12 18:05:00 +0900

    Fix test to follow previous commit

--- test/test-report-parser.rb
+++ test/test-report-parser.rb
@@ -29,12 +29,12 @@ class ReportParserTest < Test::Unit::TestCase
 2013-2-1:80%:typoが多かった
 REPORT
 
-    actual_entries = @parser.parse_stream(report_io)
+    actual_entries = @parser.parse_stream("taro", report_io)
     expected_entries = [
-      {:date => "2013-1-30", :read_ratio => "40", :comment => "特になし"},
-      {:date => "2013-2-1",  :read_ratio => "80", :comment => "typoが多かった"},
+      {:name => "taro", :date => "2013-01-30", :read_ratio => 40, :comment => "特になし"},
+      {:name => "taro", :date => "2013-02-01",  :read_ratio => 80, :comment => "typoが多かった"},
     ]
-    assert_equal(expected_entries, actual_entries)
+    assert_equal(expected_entries, actual_entries.collect(&:to_hash))
   end
 
   def test_continues_lines
@@ -45,24 +45,24 @@ REPORT
   データ駆動を使ってテストがどんどん書き直されていた
 REPORT
 
-    actual_entries = @parser.parse_stream(report_io)
+    actual_entries = @parser.parse_stream("taro", report_io)
     continues_comment = "PHPのコードを久しぶりに書いた\n" +
                           "データ駆動を使ってテストがどんどん書き直されていた"
     expected_entries = [
-      {:date => "2013-2-1", :read_ratio => "80", :comment => "typoが多かった"},
-      {:date => "2013-2-2", :read_ratio => "50", :comment => continues_comment},
+      {:name => "taro", :date => "2013-02-01", :read_ratio => 80, :comment => "typoが多かった"},
+      {:name => "taro", :date => "2013-02-02", :read_ratio => 50, :comment => continues_comment},
     ]
-    assert_equal(expected_entries, actual_entries)
+    assert_equal(expected_entries, actual_entries.collect(&:to_hash))
   end
 
   def test_zero_padding_date
     report_io = StringIO.new("2013-01-30:40%:特になし")
 
-    actual_entries = @parser.parse_stream(report_io)
+    actual_entries = @parser.parse_stream("taro", report_io)
     expected_entries = [
-      {:date => "2013-01-30", :read_ratio => "40", :comment => "特になし"},
+      {:name => "taro", :date => "2013-01-30", :read_ratio => 40, :comment => "特になし"},
     ]
-    assert_equal(expected_entries, actual_entries)
+    assert_equal(expected_entries, actual_entries.collect(&:to_hash))
   end
 
   def test_generate_daily_report
@@ -95,17 +95,17 @@ REPORT
   データ駆動を使ってテストがどんどん書き直されていた
 REPORT
 
-    actual_entries = @parser.parse_stream(report_io)
+    actual_entries = @parser.parse_stream("taro", report_io)
     continues_comment = <<-COMMENT
 PHPのコードを久しぶりに書いた
 データ駆動を使ってテストがどんどん書き直されていた
 COMMENT
     expected_entries = [
-      {:date => "2013-1-30", :read_ratio => "40", :comment => ""},
-      {:date => "2013-2-1", :read_ratio => "80", :comment => ""},
-      {:date => "2013-2-2", :read_ratio => "50", :comment => continues_comment.chomp},
+      {:name => "taro", :date => "2013-01-30", :read_ratio => 40, :comment => ""},
+      {:name => "taro", :date => "2013-02-01", :read_ratio => 80, :comment => ""},
+      {:name => "taro", :date => "2013-02-02", :read_ratio => 50, :comment => continues_comment.chomp},
     ]
 
-    assert_equal(expected_entries, actual_entries)
+    assert_equal(expected_entries, actual_entries.collect(&:to_hash))
   end
 end
