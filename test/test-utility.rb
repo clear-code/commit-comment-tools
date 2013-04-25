13c4fe67f385e1e31b6b96b3c8ca77b8aaaf6f3e
Kenji Okimoto <okimoto@clear-code.com>
2013-04-25 15:10:57 +0900

    Add test for CommitCommentTools::Utility

---
+++ test/test-utility.rb
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2013  Kenji Okimoto <okimoto@clear-code.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require "commit-comment-tools/utility"
+
+class UtilityTest < Test::Unit::TestCase
+  include CommitCommentTools::Utility
+
+  def test_calculate_percentage
+    assert_equal(1.0, calculate_percentage(10, 1000))
+  end
+
+  def test_calculate_average
+    assert_equal(10.0, calculate_average(1000, 100))
+  end
+end
