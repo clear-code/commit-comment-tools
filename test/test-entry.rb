470ec5695e1ca5a4155c764fae593ae8e833444a
Kenji Okimoto <okimoto@clear-code.com>
2013-04-25 16:18:30 +0900

    Fix indent

--- test/test-entry.rb
+++ test/test-entry.rb
@@ -18,63 +18,63 @@
 require "commit-comment-tools/entry"
 
 module CommitCommentTools
-class EntryTest < Test::Unit::TestCase
-  def test_name
-    expected_name = "yamada"
-    entry = generate_entry(:name => expected_name)
-    assert_equal(expected_name, entry.name)
-  end
+  class EntryTest < Test::Unit::TestCase
+    def test_name
+      expected_name = "yamada"
+      entry = generate_entry(:name => expected_name)
+      assert_equal(expected_name, entry.name)
+    end
 
-  data(:with_zero_padding    => "2013-02-05",
-       :without_zero_padding => "2013-2-5")
-  def test_date(date)
-    entry = generate_entry(:date => date)
-    expected_date = Date.parse("2013-02-05").strftime("%Y-%m-%d")
-    assert_equal(expected_date, entry.date)
-  end
+    data(:with_zero_padding    => "2013-02-05",
+         :without_zero_padding => "2013-2-5")
+    def test_date(date)
+      entry = generate_entry(:date => date)
+      expected_date = Date.parse("2013-02-05").strftime("%Y-%m-%d")
+      assert_equal(expected_date, entry.date)
+    end
 
-  def test_read_ratio
-    entry = generate_entry(:read_ratio => "10")
-    assert_equal(10, entry.read_ratio)
-  end
+    def test_read_ratio
+      entry = generate_entry(:read_ratio => "10")
+      assert_equal(10, entry.read_ratio)
+    end
 
-  data(:one_line           => "typoのコミットが多かった。",
-       :continues_line     => "typoのコミットが多かった。\ngit-rebaseって何？",
-       :nonexistent        => "")
-  def test_comment(comment)
-    entry = generate_entry(:comment => comment)
-    assert_equal(comment, entry.comment)
-  end
+    data(:one_line           => "typoのコミットが多かった。",
+         :continues_line     => "typoのコミットが多かった。\ngit-rebaseって何？",
+         :nonexistent        => "")
+    def test_comment(comment)
+      entry = generate_entry(:comment => comment)
+      assert_equal(comment, entry.comment)
+    end
 
-  class InvalidEntryTest < self
-    data(:no_separate    => "20130205",
-         :slash_separate => "2013/02/05",
-         :nonexistent    => "")
-    def test_date(invalid_date)
-      assert_raise(Entry::InvalidEntryError) do
-        generate_entry(:date => invalid_date)
+    class InvalidEntryTest < self
+      data(:no_separate    => "20130205",
+           :slash_separate => "2013/02/05",
+           :nonexistent    => "")
+      def test_date(invalid_date)
+        assert_raise(Entry::InvalidEntryError) do
+          generate_entry(:date => invalid_date)
+        end
+      end
+
+      data(:decimal_number => "10.5",
+           :no_number      => "NoNumber",
+           :nonexistent    => "")
+      def test_read_ratio(invalid_read_ratio)
+        assert_raise(Entry::InvalidEntryError) do
+          generate_entry(:read_ratio => invalid_read_ratio)
+        end
       end
     end
 
-    data(:decimal_number => "10.5",
-         :no_number      => "NoNumber",
-         :nonexistent    => "")
-    def test_read_ratio(invalid_read_ratio)
-      assert_raise(Entry::InvalidEntryError) do
-        generate_entry(:read_ratio => invalid_read_ratio)
-      end
+    private
+    def generate_entry(options={})
+      name       = options[:name]       || "yamada"
+      date       = options[:date]       || "2013-02-20"
+      read_ratio = options[:read_ratio] || "10"
+      comment    = options[:comment]    || "あんまり読めなかった。"
+
+      entry_chunk = "#{date}:#{read_ratio}%:#{comment}"
+      Entry.new(name, entry_chunk)
     end
   end
-
-  private
-  def generate_entry(options={})
-    name       = options[:name]       || "yamada"
-    date       = options[:date]       || "2013-02-20"
-    read_ratio = options[:read_ratio] || "10"
-    comment    = options[:comment]    || "あんまり読めなかった。"
-
-    entry_chunk = "#{date}:#{read_ratio}%:#{comment}"
-    Entry.new(name, entry_chunk)
-  end
-end
 end
