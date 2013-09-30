be1a74cb9676b6a3809d3b10b51e6e182e945857
Kenji Okimoto <okimoto@clear-code.com>
2013-09-30 17:00:11 +0900

    Implemant GnuplotScriptCommand

--- lib/commit-comment-tools/subcommands/gnuplot-script-command.rb
+++ lib/commit-comment-tools/subcommands/gnuplot-script-command.rb
@@ -18,16 +18,22 @@
 require "optparse"
 require "pathname"
 
+require "commit-comment-tools/generator/gnuplot-script"
+
 module CommitCommentTools
   module Subcommands
     class GnuplotScriptCommand < Subcommand
       def initialize
         super
+        @input_filename = nil
+        @output_filename = nil
+        @output_image_filename = nil
+        @template = :pareto
         prepare
       end
 
       def prepare
-        @parser.banner <<-BANNER
+        @parser.banner = <<-BANNER
 Usage: #{$0} [options]
   e.g: #{$0} --input-file=xxx.csv --output-file=xxx.png --template=pareto
         BANNER
@@ -35,7 +41,43 @@ Usage: #{$0} [options]
         @parser.separator("")
         @parser.separator("Options:")
 
-        @parser.on("-i", "--input-file=CSV", String)
+        @parser.on("-i", "--input-filename=FILENAME", String, "Input filename.") do |path|
+          @input_filename = Pathname(path).expand_path.to_s
+        end
+
+        @parser.on("-o", "--output-filename=FILENAME", String, "Output filename.") do |path|
+          @output_filename = Pathname(path).expand_path.to_s
+        end
+
+        @parser.on("-O", "--output-image-filename=IMAGE", String, "Output image filename.") do |path|
+          @output_image_filename = Pathname(path).expand_path.to_s
+        end
+
+        available_templates = [:pareto, :average]
+        @parser.on("-T", "--template=TEMPLATE", available_templates,
+                   "Template",
+                   "available templates: [#{available_templates.join(", ")}]",
+                   "[#{@template}]") do |template|
+          @template = template
+        end
+      end
+
+      def parse(argv)
+        super
+        option_error("Must specify --input-filename") if @input_filename.nil?
+        option_error("Must specify --output-filename") if @output_filename.nil?
+        option_error("Must specify --output-image-filename") if @output_image_filename.nil?
+      end
+
+      def exec(global_options, argv)
+        options = {
+          :input_filename        => @input_filename,
+          :output_filename       => @output_filename,
+          :output_image_filename => @output_image_filename,
+          :template              => @template
+        }
+        generator = Generator::GnuplotScript.new(options)
+        generator.generate
       end
     end
   end
